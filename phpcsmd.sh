43c41b2bc695056a1837eb18f4ceb09ae55bc1bd
Andrei Kuprienka <Andrei_Kuprienka@epam.com>
2015-06-05 01:49:13 +0000

    Allow run tests against files of set branches diff

--- phpcsmd.sh
+++ phpcsmd.sh
@@ -1,55 +1,63 @@
-#modules to check, set folders to test separated by space
-#modules=( "CatalogImportExport" "BundleImportExport" "ConfigurableImportExport" "GroupedImportExport" "ImportExport" "CatalogUrlRewrite" )
+#run cmd against the chosen files
+function phpcsmd_run {
+    FILES=($1)
+    ORIGINAL_CMD=$2
+    PLACEHOLDER=$3
 
-modules[0]="CatalogImportExport"
-modules[1]="BundleImportExport"
-modules[2]="ConfigurableImportExport"
-modules[3]="GroupedImportExport"
-modules[4]="ImportExport"
-modules[5]="CatalogUrlRewrite"
+    for FILE in "${FILES[@]}"
+    do
+        #check if directory exists
+        if [ ! -f "$FILE" ]; then
+            echo -e "\n File $FILE does not exists\n"
+            continue
+        fi
+        echo -e "*****File $FILE*****:"
+        echo -e "______________________________________________________________\nOutput:"
 
-#modules parent directories, no trailing slash
-parent_dirs[0]="app/code/Magento"
-parent_dirs[1]="dev/tests/integration/testsuite/Magento"
-parent_dirs[2]="dev/tests/api-functional/testsuite/Magento"
-parent_dirs[3]="dev/tests/static/testsuite/Magento"
+        eval ${ORIGINAL_CMD/$PLACEHOLDER/$FILE}
+
+        echo "______________________________________________________________"
+    done
+}
+
+#project root
+ROOT="."
+#parse arguments
+for i in "$@"
+do
+    case $i in
+        -b1=*|--branch1=*)
+        BRANCH1="${i#*=}"
+        ;;
+        -b2=*|--branch2=*)
+        BRANCH2="${i#*=}"
+        ;;
+        -r=*|--root=*)
+        ROOT="${i#*=}"
+        ;;
+    esac
+done
+#check if branches are set
+if [[ -z $BRANCH1 || -z $BRANCH2 ]]; then
+    echo -e "Error: branhces are not set."
+    exit 1
+fi
+#check if project root exists
+if [ ! -d "$ROOT" ]; then
+    echo -e "Error: project root doesnot exists."
+    exit 1
+fi
+
+cd $ROOT
+
+#get changed files
+CHANGED_FILES="$(git diff --name-only $BRANCH1 $BRANCH2 | sed -e 's/)$//')"
+PLACEHOLDER="|FILE|"
+PHPMD_CMD="vendor/bin/phpmd $PLACEHOLDER text dev/tests/static/testsuite/Magento/Test/Php/_files/phpmd/ruleset.xml"
+PHPCS_CMD="vendor/bin/phpcs $PLACEHOLDER --standard=dev/tests/static/testsuite/Magento/Test/Php/_files/phpcs/ruleset.xml,psr2 --warning-severity=0"
 
 echo -e "------------RUN PHPMD TESTS--------------:"
-for module in "${modules[@]}"
-do
-	for parent_dir in "${parent_dirs[@]}"
-	do
-		module_path="$parent_dir/$module"
-
-		#check if directory exists
-		if [ ! -d "$module_path" ]; then
-			echo -e "\n Module $module does not exists in $parent_dir\n"
-			continue
-		fi
-
-		echo -e "*****Module $module: ($module_path)*****:"
-		echo -e "______________________________________________________________\nOutput:"
-		vendor/bin/phpmd $module_path text dev/tests/static/testsuite/Magento/Test/Php/_files/phpmd/ruleset.xml
-		echo "______________________________________________________________"
-	done
-done
+phpcsmd_run "$CHANGED_FILES" "$PHPMD_CMD" "$PLACEHOLDER"
 
 echo -e "\n------------RUN PHPCS TESTS--------------:"
-
-for module in "${modules[@]}"
-do
-	for parent_dir in "${parent_dirs[@]}"
-	do
-		module_path="$parent_dir/$module/"
-		#check if directory exists
-        	if [ ! -d "$module_path" ]; then
-                	echo -e "\n Module $module does not exists in $parent_dir\n"
-                	continue
-        	fi
-
-		echo -e "*****Module $module: ($module_path)*****"
-                echo -e "______________________________________________________________\nOutput:"
-                vendor/bin/phpcs $module_path --standard=dev/tests/static/testsuite/Magento/Test/Php/_files/phpcs/ruleset.xml,psr2 --warning-severity=0
-                echo "______________________________________________________________"
-        done
-done
+phpcsmd_run "$CHANGED_FILES" "$PHPCS_CMD" "$PLACEHOLDER"
